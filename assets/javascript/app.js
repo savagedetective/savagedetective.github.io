bcaa7724cd6e2a409c7530407dba24f8f3194b85
Ritchie <ritch.g@gmail.com>
2019-02-16 07:53:14 -0800

    adds sticky nav and links to projects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,22 @@
 //this is just here so the file commits
+
+$(document).ready(function() {
+
+    $(window).scroll(function(){
+
+        if ($(window).scrollTop() > $("#about-me-page").height()) {
+
+            $("#navigation-bar").addClass('fixed');
+
+        } 
+        
+        else {
+
+            $("#navigation-bar").removeClass('fixed');
+        }
+        
+    //sticky nav close
+    });
+
+//document ready close
+});
